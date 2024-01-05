3defc6ee73bafb2dcfebe6755d1288cfabc70f1a
Renan Serrano <39142636+renserrano@users.noreply.github.com>
2024-01-05 00:15:02 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+// test.only()
+
+// test.only("Test de SQL Injection", async () => {
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=");
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=';");
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//   );
+// });
